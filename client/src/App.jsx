7a2ce10f4790001d9237f29de5ee39bfa8d71c3b
Safal <ajands12@gmail.com>
2026-07-30 22:47:54 +0545

    Refactor: move routes into DashboardLayouts, App.jsx is now minimal

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,41 +1,5 @@
-import { Routes, Route } from "react-router-dom";
-import { Box } from "@mui/material";
-
-import Sidebar, { DRAWER_WIDTH } from "./components/Sidebar";
-
-import Dashboard from "./pages/Dashboard";
-import Camps from "./pages/Camps";
-import Inventory from "./pages/Inventory";
-import Resources from "./pages/Resources";
-import Requests from "./pages/Requests";
-import Donations from "./pages/Donations";
-import Victims from "./pages/Victims";
-import Distribution from "./pages/Distribution";
-import Deployments from "./pages/Deployments";
-import DisasterAreas from "./pages/DisasterAreas";
+import DashboardLayouts from "./layouts/DashboardLayouts";
 
 export default function App() {
-  return (
-    <Box sx={{ display: "flex", minHeight: "100vh" }}>
-      <Sidebar />
-
-      <Box
-        component="main"
-        sx={{ flexGrow: 1, bgcolor: "#f1f5f9", minHeight: "100vh", width: `calc(100% - ${DRAWER_WIDTH}px)` }}
-      >
-        <Routes>
-          <Route path="/"               element={<Dashboard />} />
-          <Route path="/disaster-areas" element={<DisasterAreas />} />
-          <Route path="/camps"          element={<Camps />} />
-          <Route path="/resources"      element={<Resources />} />
-          <Route path="/inventory"      element={<Inventory />} />
-          <Route path="/victims"        element={<Victims />} />
-          <Route path="/requests"       element={<Requests />} />
-          <Route path="/donations"      element={<Donations />} />
-          <Route path="/distribution"   element={<Distribution />} />
-          <Route path="/deployments"    element={<Deployments />} />
-        </Routes>
-      </Box>
-    </Box>
-  );
+  return <DashboardLayouts />;
 }
